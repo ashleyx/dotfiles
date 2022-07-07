f7fd1311d7ec0489969f98f76f59efb807ced272
Ashley Xavier <ashleythomasxavier@gmail.com>
2022-07-07 22:38:36 +0530

    tmux setup change

--- zsh/.config/zsh/.tmux-setup.sh
+++ zsh/.config/zsh/.tmux-setup.sh
@@ -8,12 +8,9 @@ exists=$(tmux list-sessions 2>/dev/null| grep $session1)
 if [ "$exists" = "" ]
 then
 	tmux new-session -d -s $session1 -n 'Code'
-	tmux send-keys -t $session1:Code.1 'cd ~/dotfiles && nvim' C-m
-	#tmux split-window -h -t $session1
-	#tmux send-keys -t $session1:Code.2 'python' C-m
-	#tmux select-pane -t $session1:Code.1
+	tmux send-keys -t $session1:Code.1 'cd ~/dotfiles' C-m
 	tmux new-window -t $session1
-	tmux send-keys -t $session1:2.1 'macchina' C-m
+	tmux send-keys -t $session1:2.1 'cd ~/git' C-m
 	return
 fi
 
@@ -30,14 +27,14 @@ fi
 exists=$(tmux list-sessions 2>/dev/null| grep $session3)
 if [ "$exists" = "" ]
 then
-	#B2B
+	# B2B
 	tmux new-session -d -s $session3 -n 'B2B'
-	#B2B DEV 
+	# B2B DEV 
 	tmux send-keys -t $session3:B2B.1 'ssh epitome-dev-b2b-server' C-m
 	tmux send-keys -t $session3:B2B.1 'cd epitome-parser' C-m
 	tmux send-keys -t $session3:B2B.1 'docker-compose logs -f --tail=100' C-m
 	tmux select-pane -t $session3:B2B.1 -T 'DEV'
-	#B2B PROD
+	# B2B PROD
 	tmux split-window -h -t $session3
 	tmux send-keys -t $session3:B2B.2 'ssh epitome-prod-b2b-server' C-m
 	tmux send-keys -t $session3:B2B.2 'cd epitome-parser' C-m
