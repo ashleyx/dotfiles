42fa8b8a825e8f3c4981d3ce14a3f633c82ea7ea
Ashley Xavier <ashleythomasxavier@gmail.com>
2022-07-26 15:21:47 +0530

    added check if volume is correctly mounted

--- backup_script.sh
+++ backup_script.sh
@@ -2,10 +2,12 @@
 
 #@weekly rm -rf /home/ashleyx/Downloads/*
 #@daily sh /home/ashleyx/dotfiles/backup_script.sh
-
-rsync -r /home/ashleyx/git /mnt/external/backups/
-rsync -r /home/ashleyx/dotfiles /mnt/external/backups/
-rsync -r /home/ashleyx/work /mnt/external/backups/
-rsync -r /home/ashleyx/Pictures /mnt/external/backups/
-rsync -r /home/ashleyx/Documents /mnt/external/backups/
-rsync -r /home/ashleyx/Videos /mnt/external/backups/
+if findmnt --target /mnt/external --source /dev/mapper/externaldrive >/dev/null
+then
+	rsync -r /home/ashleyx/git /mnt/external/backups/
+	rsync -r /home/ashleyx/dotfiles /mnt/external/backups/
+	rsync -r /home/ashleyx/work /mnt/external/backups/
+	rsync -r /home/ashleyx/Pictures /mnt/external/backups/
+	rsync -r /home/ashleyx/Documents /mnt/external/backups/
+	rsync -r /home/ashleyx/Videos /mnt/external/backups/
+fi
