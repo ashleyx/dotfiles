7f66a0711c81c822e0a8610cf605b4a4753f0254
Ashley Xavier <ashleythomasxavier@gmail.com>
2023-02-03 18:30:39 +0530

    added alias for ip

--- post_install_arch.sh
+++ post_install_arch.sh
@@ -1,7 +1,7 @@
 sudo pacman -S nvim tmux firefox zsh kitty waybar wofi dunst exa zoxide jq\
-	swaybg btop nvtop tumbler --noconfirm
+	swaybg btop nvtop tumbler bluez bluez-utils ripgrep rsync --noconfirm
 
-yay -S networkmanager-iwd grimshot clipman nerd-fonts slack-desktop postman-bin signal-desktop spotify ncpamixer-git --noconfirm 
+yay -S networkmanager-iwd grimshot clipman nerd-fonts ttf-fira-code slack-desktop postman-bin signal-desktop spotify ncpamixer-git bluetuith --noconfirm 
 yay -S xdg-desktop-portal-wlr-git bemenu-wayland 
 mkdir ~/installs
 git git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/installs/zsh-autocomplete
