aa53e82ced90a7450ab63552be3de95f7dbc0f3d
Ashley Xavier <ashleythomasxavier@gmail.com>
2024-11-28 11:51:44 +0530

    minor hyprland config changes, new aliases, brave executable fix

--- post_install_arch.sh
+++ post_install_arch.sh
@@ -1,5 +1,5 @@
 sudo pacman -S nvim pipewire-pulse networkmanager tmux firefox zsh kitty waybar wofi dunst exa zoxide jq\
-	swaybg btop nvtop tumbler bluez bluez-utils ripgrep rsync cronie --noconfirm
+	swaybg btop nvtop tumbler bluez bluez-utils ripgrep rsync cronie net-tools --noconfirm
 
 yay -S grimshot clipman nerd-fonts ttf-fira-code \ 
 	spotify bluetuith swaylock-effects --noconfirm 
